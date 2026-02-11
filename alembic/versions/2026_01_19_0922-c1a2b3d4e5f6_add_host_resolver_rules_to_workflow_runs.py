6977e6bd981f1ff2b894cb96fa60c31ec6b90d91
Kamil Ujec <kamil.ujec@gmail.com>
2026-02-11 09:26:13 +0100

    Poprawienie migracji

--- alembic/versions/2026_01_19_0922-c1a2b3d4e5f6_add_host_resolver_rules_to_workflow_runs.py
+++ alembic/versions/2026_01_19_0922-c1a2b3d4e5f6_add_host_resolver_rules_to_workflow_runs.py
@@ -1,7 +1,7 @@
 """add_host_resolver_rules_to_workflow_runs
 
 Revision ID: c1a2b3d4e5f6
-Revises: b4738bd17198
+Revises: 43217e31df12
 Create Date: 2026-01-19 09:22:00.000000+00:00
 
 """
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "c1a2b3d4e5f6"
-down_revision: Union[str, None] = "b4738bd17198"
+down_revision: Union[str, None] = "43217e31df12"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
